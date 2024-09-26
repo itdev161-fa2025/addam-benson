91b0f24e7cfc32320e3cddc293812bd3e5074d93
Addam Benson <addam.benson@gmail.com>
2024-09-26 15:51:02 -0500

    remove unnecessary files

--- highscores.js
+++
@@ -1,6 +0,0 @@
-const highScoresList = document.getElementById("high-scores-list");
-const highScores = JSON.parse(localStorage.getItem("high scores")) || [];
-
-highScoresList.innerHTML = highScores.map(score => {
-    return `<li class="high-score">${score.name} - ${score.score}</li>`
-}).join("");
